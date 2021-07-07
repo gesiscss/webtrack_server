e5c32093595739d60b23353a67312a8c4bfde9ef
Roberto Ulloa <roberto.ur@gmail.com>
2021-07-07 14:44:04 +0200

    update scripts so they use the last json to open the last html

--- open_html.sh
+++ open_html.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-lynx storage/html/`ls -Art storage/html/ | tail -n $1 | head -n 1`
+lynx storage/html/`ls -Art storage/html/ | tail -n $1 | head -n 1 | sed 's/html/json/g'`
--- open_plain.sh
+++ open_plain.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat storage/html/`ls -Art storage/html/ | tail -n $1 | head -n 1`
+cat storage/html/`ls -Art storage/html/ | tail -n $1 | head -n 1 | sed 's/html/json/g'`
